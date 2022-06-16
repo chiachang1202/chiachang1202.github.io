f7860261d19946ef67a801ca2e01d617202ff7f5
Li, Chia-Chang <hmtp45563@gmail.com>
2022-06-16 14:27:20 +0800

    Migrate build tool to Parcel.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
